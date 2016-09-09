a17fc121cee6a694bdf613719303bf406c411e5b
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-09-09 13:32:47 +0200

    WIP

--- tasks/generate-version.sh
+++ tasks/generate-version.sh
@@ -10,9 +10,6 @@ cp -r ../version/. ./
 echo "Bump to ${VERSION}"
 echo "${VERSION}" > version
 
-# Specific for Github - should be fixed in the docker image
-ssh-keyscan github.com ~/.ssh/known_hosts
-
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
 git add version
