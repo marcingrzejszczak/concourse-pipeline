c49f9031416305cb3a40a1cea7084ece608ae8d7
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-09-09 14:45:12 +0200

    WIP

--- tasks/generate-settings.sh
+++ tasks/generate-settings.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-source pipeline.sh
+export ROOT_FOLDER=$( pwd )
+export REPO_RESOURCE=repo
+export SCRIPTS_OUTPUT_FOLDER=${ROOT_FOLDER}/${REPO_RESOURCE}/ciscripts
+export TOOLS_RESOURCE=tools
+export VERSION_RESOURCE=version
+export OUTPUT_RESOURCE=output
+
+echo "Root folder is [${ROOT_FOLDER}]"
+echo "Repo resource folder is [${REPO_RESOURCE}]"
+echo "Tools resource folder is [${TOOLS_RESOURCE}]"
+echo "Version resource folder is [${VERSION_RESOURCE}]"
+echo "Scripts will be copied to [${SCRIPTS_OUTPUT_FOLDER}]"
+
+source "${TOOLS_RESOURCE}"/tasks/pipeline.sh
 
 mkdir -p ~/.m2/
 
+echo "Writing settings xml"
+
+set +x
 cat > ~/.m2/settings.xml <<EOF
 <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
@@ -18,3 +34,6 @@ cat > ~/.m2/settings.xml <<EOF
       </servers>
 </settings>
 EOF
+set -x
+
+echo "Settings xml written"
