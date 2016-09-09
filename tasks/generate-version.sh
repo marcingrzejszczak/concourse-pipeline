3b1ed604253fcdc01d710f9a6c8bdb0c63909386
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-09-09 13:22:44 +0200

    WIP

--- tasks/generate-version.sh
+++ tasks/generate-version.sh
@@ -4,15 +4,13 @@
 VERSION=1.0.0.M1
 MESSAGE="[Concourse CI] Bump to Next Version ($VERSION)"
 
-ls -al version/
 cd out
 
 cp -r ../version/. ./
-ls -al
 echo "Bump to ${VERSION}"
 echo "${VERSION}" > version
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
 git add version
 git commit -m "${MESSAGE}"
-git push
+git push origin version
