75073848cfea7391c1d93b4b2488d424b15f04ca
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-09-09 14:28:41 +0200

    WIP

--- tasks/test-deploy.sh
+++ tasks/test-deploy.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-source pipeline.sh
+export ROOT_FOLDER=$( pwd )
+export REPO_RESOURCE=repo
+export SCRIPTS_OUTPUT_FOLDER=${ROOT_FOLDER}/${REPO_RESOURCE}/ciscripts
+export TOOLS_RESOURCE=tools
+export VERSION_RESOURCE=version
+export OUTPUT_RESOURCE=output
+
+echo "Root folder is [${ROOT_FOLDER}]"
+echo "Repo resource folder is [${REPO_RESOURCE}]"
+echo "Tools resource folder is [${TOOLS_RESOURCE}]"
+echo "Version resource folder is [${VERSION_RESOURCE}]"
+echo "Scripts will be copied to [${SCRIPTS_OUTPUT_FOLDER}]"
+
+source "${TOOLS_RESOURCE}"/tasks/pipeline.sh
 
 echo "Testing the built application on test environment"
 . ./${SCRIPTS_OUTPUT_FOLDER}/test_deploy.sh
