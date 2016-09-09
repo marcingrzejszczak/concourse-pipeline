b0ed15ac7681da0a908ad44383f13a2cce4d8ef1
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-09-09 16:56:10 +0200

    WIP

--- tasks/build-and-upload.sh
+++ tasks/build-and-upload.sh
@@ -13,19 +13,20 @@ echo "Tools resource folder is [${TOOLS_RESOURCE}]"
 echo "Concourse resource folder is [${CONCOURSE_RESOURCE}]"
 echo "Version resource folder is [${VERSION_RESOURCE}]"
 
-source ${CONCOURSE_RESOURCE}/tasks/pipeline.sh
+source ${ROOT_FOLDER}/${CONCOURSE_RESOURCE}/tasks/pipeline.sh
 
 echo "Generating settings.xml for Maven in local m2"
-. ${CONCOURSE_RESOURCE}/tasks/generate-settings.sh
+. ${ROOT_FOLDER}/${CONCOURSE_RESOURCE}/tasks/generate-settings.sh
 
 echo "Building and uploading the projects artifacts"
 cd ${REPO_RESOURCE}
-# settings.xml should come with the box
+
+# settings.xml should come with the box, I can't deploy the artifacts
 #./mvnw clean verify deploy -Dversion=${PIPELINE_VERSION}
 ./mvnw clean verify install -Dversion=${PIPELINE_VERSION}
 
 echo "Tagging the project with dev tag"
-echo "dev/${PIPELINE_VERSION}" > ${REPO_RESOURCE}/target/tag
+echo "dev/${PIPELINE_VERSION}" > ${ROOT_FOLDER}/${REPO_RESOURCE}/target/tag
 
 # WE CAN USE OUTPUT TO ARCHIVE THE RESULTS?
 #mkdir -p ${OUTPUT_RESOURCE}/junit
